1d9adf887d31d1435510a2107fa5d614aca2fa3a
Animesh-Jantwal <animeshjantwalcoding@gmail.com>
2025-08-09 23:43:40 +0530

    Completed the async and await function with the dom manupulation  to show the data on the website

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const api_key = "=91786ef1ff46d285feec730a53b2a028&units";
-const api_url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=dehradun";
+const api_key = "91786ef1ff46d285feec730a53b2a028";  
+const api_url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=dehradun&appid=" + api_key;
 
 
-async function checkweather(params) {
+async function checkweather() {
     const response = await fetch(api_url + `&appid =${api_key}`);
     var data = await response.json();
+    console.log(data);
+
+
+document.querySelector(".City").innerHTML = data.name;
+document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+document.querySelector(".wind").innerHTML = data.wind.speed + " m/s";
 }
+
+checkweather();
