b672705e74a27e59153b0ddad77773d9d76d331f
Animesh-Jantwal <animeshjantwalcoding@gmail.com>
2025-08-09 23:26:25 +0530

    Made the async and await function for getting and storing the data using url and api key

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const api_key = "=91786ef1ff46d285feec730a53b2a028&units";
-const api_url = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+const api_url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=dehradun";
 
 
+async function checkweather(params) {
+    const response = await fetch(api_url + `&appid =${api_key}`);
+    var data = await response.json();
+}
